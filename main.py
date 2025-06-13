e14973faffb78b5200e07ec560dfc752664ff51c
FAWAJ SURAIM <fawajsuraim@gmail.com>
2025-06-14 05:14:03 +0600

    main

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 print("I like Pizza")
 print("It is good")
 print("I want be a great Engineer")
+
+name = "SURAIM"
+food = "Biriani"
+email = "fawajsuraim@gmail.com"
+
+print(name)
+print(type(name))
+print(f"Hello {name}") # it is a f string where a variable can be used inside a string
+print(f"Your name is {name}")
+print(f"Your favourite foof is: {food}")
+print(f"Your email is : {email}")
